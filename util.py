f5cd8abd2aef70873bfbc8afda642261c3921ff6
runningwolf666 <runningwolf2016@163.com>
2015-08-07 18:48:14 +0800

    Create util.py

---
+++ util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+请在Python3下运行此程序='Please run this program with Python3'
+import re
+
+# http://stackoverflow.com/questions/2545532/python-analog-of-natsort-function-sort-a-list-using-a-natural-order-algorithm
+def natural_key(string_):
+    """
+    See http://www.codinghorror.com/blog/archives/001018.html
+    list_ = ['img1', 'img12', 'img5', 'img13', 'img2']
+    print(sorted(list_, key=natural_key))
+    """
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
